a6e22a6df631b2ff7c32c5836611ffeee5ac9a6d
cyla00 <cyla00@protonmail.com>
2020-02-25 11:57:20 +0100

    base temp.

---
+++ random.js
@@ -0,0 +1,23 @@
+const nomi = ['lol', 'lil', 'lal'];
+
+function randomItem() {
+  console.log(nomi[Math.floor(Math.random()*nomi.lenght)]);
+}
+
+
+
+var arr = ["GFG_1", "GeeksForGeeks",
+                "Geeks", "Computer Science Portal"];
+
+                function GFG_Fun() {
+          console.log(arr[Math.floor(Math.random() * arr.length)]) ;
+
+       }  
+
+// arr[Math.floor(Math.random() * arr.length)]
+
+
+// const random = (nomi) => {
+//   nomi[Math.floor(Math.number()*nomi.lenght)];
+// }
+// return(random);
